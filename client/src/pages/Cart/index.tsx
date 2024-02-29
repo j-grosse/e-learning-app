9c25cbfdfb9c690e795564f0f7b960c2339c3959
ernest-lamptey <1ernestlamptey@gmail.com>
2024-02-29 20:51:24 +0100

    add remove feature on cart items

--- client/src/pages/Cart/index.tsx
+++ client/src/pages/Cart/index.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
-import { useCart } from '../../context/CartContext'
+import { useCart, useRemoveFromCart } from '../../context/CartContext'
 import { useNavigate } from 'react-router-dom';
+import { FaCross, FaWindowClose } from 'react-icons/fa';
 
 const index = () => {
   const selectedCourses = useCart();
+  const removeFromCart = useRemoveFromCart();
   const navigate = useNavigate();
 
   const handleClick = () => {
     navigate('/dashboard/checkout')
   }
 
+
   return (
     <div>
         { selectedCourses && selectedCourses.map((item, index) => (
-            <p key={index}>{item.title} - By {item.tutor}</p>
+            <div key={index} className='flex gap-4'>
+              <span>{index + 1}. {item.title} - By {item.tutor}</span>
+              <FaWindowClose onClick={() => removeFromCart(item.id)} color='red'/>
+          </div>
         ))}
         <button onClick={handleClick} className="flex items-center justify-center w-full gap-3 p-3 mb-4 text-white bg-blue-500 rounded-md hover:bg-orange-400 hover:text-black">Checkout</button>
     </div>
--- client/src/pages/Checkout/CartDetails.tsx
+++ client/src/pages/Checkout/CartDetails.tsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import { useCart } from '../../context/CartContext';
+import { useCart, useRemoveFromCart } from '../../context/CartContext';
+import { FaCross, FaWindowClose } from 'react-icons/fa';
 
 const CartDetails = () => {
   const selectedCourses = useCart();
+  const removeFromCart = useRemoveFromCart();
   
   return (
     <div className='mb-4'>
         <h3 className='font-semibold text-xl'>Cart Items</h3>
           { selectedCourses && selectedCourses.map((item, index) => (
-            <p key={index}>{index + 1}. {item.title} - By {item.tutor}</p>
+            <div key={index} className='flex gap-4'>
+            <span>{index + 1}. {item.title} - By {item.tutor}</span>
+            <FaWindowClose onClick={() => removeFromCart(item.id)} color='red'/>
+        </div>
         ))}
     </div>
   )
