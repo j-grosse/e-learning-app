b283b5c2e7dd013ae75e38c41bd8e7f09cda7933
j-grosse <103175813+j-grosse@users.noreply.github.com>
2024-03-05 20:08:29 +0100

    Added CourseView w/ course index & lesson content

--- client/src/context/CoursesContext.tsx
+++ client/src/context/CoursesContext.tsx
@@ -8,36 +8,41 @@ export const CoursesContext = createContext(null);
 const CoursesProvider = ({ children }) => {
   const [courses, setCourses] = useState(null);
 
+  const loadMockarooData = () => {
+    axios
+      .get(`https://my.api.mockaroo.com/courses.json?key=2030e670`)
+      .then((res) => {
+        setCourses(res.data);
+        console.log('axios request to Mockaroo.com:', '\n\n', res.data);
+      })
+      .catch((error) => {
+        if (error.response && error.response.status === 500) {
+          console.log(
+            '500 Internal Server Error: Probably no more requests to Mockaroo.com allowed today.'
+          );
+        } else {
+          console.log(error.message);
+        }
+      });
+  };
+  // const loadMongoData = () => {
+  //   axios
+  //     .get(`/api/courses`)
+  //     .then((res) => {
+  //       setCourses(res.data);
+  //       console.log('axios data from MongoDB Atlas:', '\n\n', res.data);
+  //     })
+  //     .catch((e) => console.log(e));
+  // };
+
   useEffect(() => {
-    const loadMockarooData = () => {
-      axios
-        .get(`https://my.api.mockaroo.com/courses.json?key=2030e670`)
-        .then((res) => {
-          setCourses(res.data);
-          console.log('axios data from Mockaroo.com:', '\n\n', res.data);
-        })
-        .catch((error) => {
-          if (error.response && error.response.status === 500) {
-            console.log("500 Internal Server Error: Probably no more requests to Mockaroo.com allowed today.");
-          } else {
-            console.log(error.message);
-          }
-        });    };
+    if (courses === null) {
+      loadMockarooData();
+    }
+  }, [courses]);
 
-    // const loadMongoData = () => {
-    //   axios
-    //     .get(`/api/courses`)
-    //     .then((res) => {
-    //       setCourses(res.data);
-    //       console.log('axios data from MongoDB Atlas:', '\n\n', res.data);
-    //     })
-    //     .catch((e) => console.log(e));
-    // };
-
-    loadMockarooData();
-    // loadMongoData();
-    // setCourses(data); // use local array data instead of database data
-  }, []);
+  // loadMongoData();
+  // setCourses(data); // use local array data instead of database data
 
   return (
     <>
