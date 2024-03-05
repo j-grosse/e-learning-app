23d488ab22815deef3fc2431f1ef7020b1475897
j-grosse <103175813+j-grosse@users.noreply.github.com>
2024-03-05 20:10:01 +0100

    Added new files for CourseView page

---
+++ client/src/pages/Dashboard/CourseView/index.tsx
@@ -0,0 +1,36 @@
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { AuthContext } from '../../../context/AuthContext.tsx';
+import { CoursesContext } from '../../../context/CoursesContext.tsx';
+import CourseIndex from './CourseIndex.tsx';
+import CourseContent from './CourseContent.tsx';
+
+const CourseLayout = () => {
+  const courses = useContext(CoursesContext);
+  const context = useContext(AuthContext);
+  // console.log('courses context:', courses);
+
+  const { id } = useParams();
+  const idNumber = parseInt(id);
+  const course = courses && courses.find((el) => el.id === idNumber);
+  console.log('course:', course);
+  console.log('user:', context.user);
+
+  return (
+    <div className="flex w-full gap-8">
+      <div className="w-3/12">
+        {course && <CourseIndex user={context.user} course={course} />}
+      </div>
+      <div className="w-9/12">
+        {/* get user's current lesson from db */}
+        {course ? (
+          <CourseContent course={course} />
+        ) : (
+          <p>Select a lesson to view its content.</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default CourseLayout;
