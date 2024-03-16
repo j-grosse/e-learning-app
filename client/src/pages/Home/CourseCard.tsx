e9f69f27c24716bd78d48282fbeac5c479e963b5
ernest-lamptey <1ernestlamptey@gmail.com>
2024-03-16 14:15:44 +0100

    remove description and borders

--- client/src/pages/Home/CourseCard.tsx
+++ client/src/pages/Home/CourseCard.tsx
@@ -6,16 +6,16 @@ const CourseCard = ({ course }) => {
   console.log('lessons:', course.lessons)
 
   return (
-    <div className="flex flex-wrap m-3 overflow-hidden rounded-lg shadow-lg bg-card">
+    <div className="flex flex-wrap m-3 overflow-hidden rounded-lg">
       <Link to={`/details/${course.id}`}>
         <div className="w-60">
           {/* Card-header */}
-          <div className="relative w-full h-32 object-fit aspect-w-1 aspect-h-1">
-            <img
-              className="object-cover w-full h-24 transition-transform duration-500 transform aspect-w-1 aspect-h-1 hover:scale-125"
-              src={course.image}
-              alt="image"
-            />
+          <div className="relative bg-yellow-300 w-full overflow-clip object-center aspect-[3/2]">
+              <img
+                className="object-cover w-full h-full transition-transform duration-500 transform hover:scale-110"
+                src={course.image}
+                alt="image"
+              />
             {/* badge: NEW */}
             {course.year == new Date().getFullYear() && (
               <span className="absolute top-0 left-0 bg-green-500 rounded-sm text-sm px-2 py-0.5 m-3">
@@ -24,15 +24,15 @@ const CourseCard = ({ course }) => {
             )}
           </div>
           {/* Card body */}
-          <div className="dark:text-foreground px-6 py-4">
+          <div className="dark:text-foreground px-2 py-2">
             {/* <p className="bg-green-100 text-green-500 px-2 py-0.5 mb-4">
               All Levels
             </p> */}
             <p className="font-light text-blue-400">{course.category}</p>
             <div className="mb-2 text-xl font-bold">{course.title}</div>
-            <p className="overflow-hidden font-light min-h-40">
+            {/* <p className="overflow-hidden font-light min-h-40">
               {course.description}
-            </p>
+            </p> */}
             <p className="mt-3 text-xl font-bold">{course.price} </p>
           </div>
           <div className="flex items-center mb-4 tutor-course-rating">
