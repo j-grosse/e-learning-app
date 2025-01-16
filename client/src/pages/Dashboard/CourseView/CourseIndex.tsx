6c8771097d48703432ffd80771dee3cb5e5caf14
j-grosse <103175813+j-grosse@users.noreply.github.com>
2025-01-16 21:08:05 +0100

    add course modules to course page

--- client/src/pages/Dashboard/CourseView/CourseIndex.tsx
+++ client/src/pages/Dashboard/CourseView/CourseIndex.tsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
+// import axios from 'axios';
 import {
   Accordion,
   AccordionContent,
@@ -9,79 +9,95 @@ import {
 import ProgressDemo from '@/components/ProgressDemo';
 import { Link } from 'react-router-dom';
 
+/* The CourseIndex component displays a table of content / list of lessons for a given course.
+ * It fetches lesson data from an API and displays it using an accordion UI.
+ * It also displays a progress bar to show the user's progress in the course.
+ */
+
 const CourseIndex = ({ user, course, setSelectedLesson }) => {
-  const [lessons, setLessons] = useState(course.lessons);
+  // const [lessons, setLessons] = useState(course.lessons);
 
-  useEffect(() => {
-    const fetchData = () => {
-      axios
-        .get(`/api/lessons/65f075d84c6ccdf6f54d124f`)
-        .then((res) => console.log('lessons data:', res.data))
-        // .then((res) => setLessons(res.data))
-        .catch((e) => console.log(e.message));
-    };
-    fetchData();
-    // console.log(lessons);
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = () => {
+  //     axios
+  //       .get(`/api/lessons/65f075d84c6ccdf6f54d124f`)
+  //       .then((res) => console.log('lessons data:', res.data))
+  //       // .then((res) => setLessons(res.data))
+  //       .catch((e) => console.log(e.message));
+  //   };
+  //   fetchData();
+  //   // console.log(lessons);
+  // }, []);
+
   console.log(course);
-  lessons && console.log('course lessons:', lessons);
-  return (
-    <div>
-      <h2>{course.title}</h2>
-      <br />
-      <h3>Units</h3>
-      {/* each unit should contain lessons that are not collabsible */}
+  // lessons && console.log('course lessons:', lessons);
 
-      <Accordion type="single" collapsible>
-        {lessons &&
-          lessons.map((lesson, index) => (
-            <React.Fragment key={lesson._id}>
-              <br />
-              <ProgressDemo value={33} />
-              <AccordionItem value={`item-${index + 1}`}>
-                <AccordionTrigger>
-                  <Link to={`/lessons/${lesson._id}`}>{lesson.title}</Link>
-                  {/* <p onClick={setSelectedLesson(lesson._id)}>{lesson.title}</p> */}
-                </AccordionTrigger>
-                <AccordionContent key={lesson._id}>
-                  {/* <Link to={`/lessons/${lesson._id}`}> */}
-                  {/* <Link to={`/lessons/${lesson._id}/${section._id}`}> */}
-                  {/* </Link> */}
-                  {lesson.text}
-                </AccordionContent>
-              </AccordionItem>
-            </React.Fragment>
-          ))}
-      </Accordion>
-      {/* <Accordion type="single" collapsible>
-        <br />
-        <ProgressDemo value={33} />
-        <AccordionItem value="item-1">
-          <AccordionTrigger><Link to="1">Lesson 1</Link></AccordionTrigger>
-          <AccordionContent><Link to="1.1">Introduction</Link></AccordionContent>
-          <AccordionContent><Link to="1.2">Overview</Link></AccordionContent>
-          <AccordionContent><Link to="1.3">learning</Link></AccordionContent>
-        </AccordionItem>
-        <br />
-        <ProgressDemo value={33} />
-        <AccordionItem value="item-2">
-          <AccordionTrigger><Link to="2">Lesson 2</Link></AccordionTrigger>
-          <AccordionContent>Introduction</AccordionContent>
-        </AccordionItem>
-        <br />
-        <ProgressDemo value={33} />
-        <AccordionItem value="item-3">
-          <AccordionTrigger><Link to="3">Lesson 3</Link></AccordionTrigger>
-          <AccordionContent>Introduction</AccordionContent>
-        </AccordionItem>
-        <br />
-        <ProgressDemo value={33} />
-        <AccordionItem value="item-4">
-          <AccordionTrigger><Link to="4">Lesson 4</Link></AccordionTrigger>
-          <AccordionContent>Introduction</AccordionContent>
-        </AccordionItem>
-      </Accordion> */}
-    </div>
+  if (!course) return <div>Loading...</div>;
+
+  return (
+    <>
+      <h2>{course.title}</h2>
+      <ProgressDemo value={33} />
+
+      {course._id}
+      <img src={course.image} alt={course.title} />
+      <p>{course.description}</p>
+      <br />
+
+      {course.courseModules.map((module) => (
+        <div key={module._id}>
+          <h3>{module.title}</h3>
+          {module.lessons &&
+            (module.lessons.length > 1 ? (
+              module.lessons.map((lesson) => (
+                <Accordion key={lesson._id} type="single" collapsible>
+                  <p onClick={() => setSelectedLesson(lesson._id)}>
+                    {lesson.text}
+                  </p>
+                  <React.Fragment key={lesson._id}>
+                    <br />
+                    <AccordionItem value={lesson._id}>
+                      <AccordionTrigger>{lesson.title}</AccordionTrigger>
+                      <AccordionContent key={lesson._id}>
+                        <p>{lesson.description}</p>
+                        <Link
+                          to={`/lessons/${lesson._id}`}
+                          onClick={() => setSelectedLesson(lesson._id)}
+                        >
+                          {lesson.text}
+                        </Link>
+                      </AccordionContent>
+                    </AccordionItem>
+                  </React.Fragment>
+                </Accordion>
+              ))
+            ) : (
+              <Accordion type="single" collapsible>
+                <p onClick={() => setSelectedLesson(module.lessons[0]._id)}>
+                  {module.lessons[0].text}
+                </p>
+                <React.Fragment key={module.lessons[0]._id}>
+                  <br />
+                  <AccordionItem value={module.lessons[0]._id}>
+                    <AccordionTrigger>
+                      {module.lessons[0].title}
+                    </AccordionTrigger>
+                    <AccordionContent key={module.lessons[0]._id}>
+                      <p>{module.lessons[0].description}</p>
+                      <Link
+                        to={`/lessons/${module.lessons[0]._id}`}
+                        onClick={() => setSelectedLesson(module.lessons[0]._id)}
+                      >
+                        {module.lessons[0].text}
+                      </Link>
+                    </AccordionContent>
+                  </AccordionItem>
+                </React.Fragment>
+              </Accordion>
+            ))}
+        </div>
+      ))}
+    </>
   );
 };
 
