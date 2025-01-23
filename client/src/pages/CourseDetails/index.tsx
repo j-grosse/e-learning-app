1675682b59678ab335cb8cabeec767462eea05f3
j-grosse <103175813+j-grosse@users.noreply.github.com>
2025-01-23 12:17:52 +0100

    add scroll-to-top functionality

--- client/src/pages/CourseDetails/index.tsx
+++ client/src/pages/CourseDetails/index.tsx
@@ -1,13 +1,14 @@
-import React, { useContext } from 'react';
+import { useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import CourseHeader from './CourseHeader';
 import CourseAbout from './CourseAbout';
 import CourseSidebar from './CourseSidebar';
 // import data from '../../assets/courseData';
 import { CoursesContext } from '../../context/CoursesContext';
+import ScrollToTop from '@/components/ui/scroll-to-top';
 
 const CourseDetails = () => {
-  const {courses, loading} = useContext(CoursesContext);
+  const { courses, loading } = useContext(CoursesContext);
   console.log('courses context:', courses);
 
   const { id } = useParams();
@@ -16,15 +17,13 @@ const CourseDetails = () => {
   // console.log('course:', course);
 
   if (loading) return <p>Loading...</p>;
-
+  
   return (
     <div className="bg-gray-50 dark:bg-black">
-      Home / courses / {course.title}
+      <ScrollToTop />
       <CourseHeader course={course} />
-      <div className="flex">
-        <div className="container">
-          <CourseAbout course={course} />
-        </div>
+      <div className="flex flex-col md:flex-row mt-4">
+        <CourseAbout course={course} />
         <CourseSidebar course={course} />
       </div>
     </div>
